05b3607dcd4279dc061cba8d07554b4ed7f5f2ab
Daniel Gonzalez <danielgonzalez@Daniels-MacBook-Pro.local>
2023-11-30 15:28:37 -0600

    rebuilding lost home css file

--- src/routes/home.js
+++ src/routes/home.js
@@ -13,9 +13,9 @@ function Home() {
     ];
 
     // Function to render images
-    const renderImages = () => {
+    const renderKids = () => {
         return imageUrls.map((url, index) => (
-            <img id="homepageImages" key={index} src={url} alt={`Image ${index + 1}`} />
+            <img className="kidsImages" key={index} src={url} alt={`Image ${index + 1}`} />
         ));
     };
 
@@ -30,34 +30,34 @@ function Home() {
     // Function to render teacher images
     const renderTeachers = () => {
         return teacherUrls.map((url, index) => (
-            <img id="homepageTeachers" key={index} src={url} alt={`Image ${index + 1}`} />
+            <img className="teacherImages" key={index} src={url} alt={`Image ${index + 1}`} />
         ));
     };
 
     return (
         <body>
             <div>
-                <div id="titleAreaPMessage">
-                    <div id="titleArea">
-                        <div id="title">
-                            <h1 id="companyName">Rising Stars Bilingual Learning Center</h1>
+                <div className="titleAreaPMessage">
+                    <div className="titleArea">
+                        <div className="title">
+                            <h1 className="companyName">Rising Stars Bilingual Learning Center</h1>
                         </div>
-                        <div id="smallText">
-                            <p id="smallText">Where Little Ones Shine Bright: Nurture, Learn, and Grow at Rising Stars - Where Smart Futures Begin!</p>
+                        <div className="smallText">
+                            <p className="smallText">Where Little Ones Shine Bright: Nurture, Learn, and Grow at Rising Stars - Where Smart Futures Begin!</p>
                         </div>
                     </div>
-                    <div id="smallText">
+                    <div className="smallText">
                         <h2>1.3k bright stars</h2>
-                        <p id="smallText">graduated from our nurturing daycare</p>
+                        <p className="smallText">graduated from our nurturing daycare</p>
                     </div>
                 </div>
-                <div id="images-container">
-                    {renderImages()}
+                <div className="images-container">
+                    {renderKids()}
                 </div>
-                <div id="generalOneContainer">
+                <div className="generalOneContainer">
                     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <p id="generalOneTitle">ABOUT US</p>
-                        <div id="teacherArea">
+                        <p className="generalOneTitle">ABOUT US</p>
+                        <div className="teacherArea">
                             {renderTeachers()}
                             <div style={{color: 'white', paddingLeft: '5px'}}>
                                 <p>Meet the</p>
@@ -67,15 +67,15 @@ function Home() {
                     </div>
                     <div style={{display: 'flex', flexDirection: 'column', marginLeft: '60px'}}>
                         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                            <div id='daycareData'>
+                            <div className='daycareData'>
                                 <h1 style={{fontSize: '50px'}}>8+</h1>
                                 <p>Years of dedicated trusted care and early childhood education since establishment</p>
                             </div>
-                            <div id='daycareData'>
+                            <div className='daycareData'>
                                 <h1 style={{fontSize: '50px'}}>4</h1>
                                 <p>Classrooms, tailored learning environments designed for each developmental stage.</p>
                             </div>
-                            <div id='daycareData'>
+                            <div className='daycareData'>
                                 <h1 style={{fontSize: '50px'}}>6</h1>
                                 <p>Teachers dedicated and committed to nurturing young minds and fostering growth.</p>
                             </div>
