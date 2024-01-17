035a41796c99e7876acf2deffe2184ebb7678d43
dpolygon <dannyg@lonestardev.net>
2024-01-17 02:01:52 -0800

    added some review functionality

--- src/routes/home.js
+++ src/routes/home.js
@@ -12,6 +12,21 @@ import './home.css'
 function Home() {
     const [program, setProgram] = useState('Infants');
     const [reviewsData, setReviewsData] = useState(reviewData);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const animatedElement = document.getElementById('rev');
+        animatedElement.classList.add('blurReview');
+
+        const timer = setTimeout(() => {
+            animatedElement.classList.remove('blurReview');
+          // Update the state after a delay (e.g., 3 seconds)
+          setCount(prevCount => prevCount === 2 ? 0 : prevCount + 1);
+        }, 10000); // Set the delay in milliseconds (3 seconds in this example)
+    
+        // Cleanup the timer when the component unmounts
+        return () => clearTimeout(timer);
+    }, [count]);
 
     const getYelpReviews = () => {
 
@@ -51,11 +66,15 @@ function Home() {
                     <ProgramInfo selectedProgram={program}/>
                 </div>
 
-                <div className='reviewsArea'>
+                <div className='reviewsArea' style={{marginTop: '200px'}}>
                     <div className='reviewsContainer'>
-                        <div className="reviews">
-                            <p style={{padding: '8px 20px'}}>{reviewsData.reviews[0].user.name}</p>
-                            <p>{reviewsData.reviews[0].text}</p>
+                        <div id='rev' className="reviews" 
+                            onClick={() => { const urlToOpen = reviewsData.reviews[count].url;
+                                             window.open(urlToOpen, '_blank');}}>
+                            <img src={reviewsData.reviews[count].user.image_url} style={{borderRadius: '50%', height: '80px', width: '80px'}}></img>
+                            <p style={{padding: '8px 20px'}}>{reviewsData.reviews[count].user.name}</p>
+                            <p>⭐️⭐️⭐️⭐️⭐️</p>
+                            <p>{reviewsData.reviews[count].text}</p>
                         </div>
                     </div>
                     <h1 style={{color: 'white'}}>Hear from Happy Parents!</h1>
