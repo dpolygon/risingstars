d49ac8d619a6a030845e7768a87ea6534d3c491f
Daniel Gonzalez <danielgonzalez@Daniels-MacBook-Pro.local>
2024-01-30 17:35:58 -0800

    adding more to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
+import { IoLogoFacebook } from "react-icons/io";
+import { ImYelp } from "react-icons/im";
+
 export default function Footer() {
   return (
     <div style={{paddingTop: '3rem'}}>
-        <div style={{width: 'auto', height: '20vh', backgroundColor: 'black'}}>
-
+        <div style={{width: 'auto', 
+                    height: '25vh', 
+                    backgroundColor: 'black',
+                    padding: '4%',
+                    color: 'white', 
+                    display: 'flex', 
+                    flexDirection: 'row', 
+                    alignContent: 'center',
+                    justifyContent: 'space-between', 
+                    alignItems: 'center'}}>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <p>Rising Stars</p>
+                <p>Bilingual Learning Center</p>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                    <IoLogoFacebook />
+                    <ImYelp/>
+                    <p>LoneStarDev</p>
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <p>About</p>
+                <p>Contact</p>
+                <p>Visit Us</p>
+            </div>
+            <div>
+            <p>11406 Conroy Ln</p>
+                <p>Manchaca, Texas</p>
+                <p>+1(737)226-0768</p>
+            </div>
         </div>
     </div>
   )
