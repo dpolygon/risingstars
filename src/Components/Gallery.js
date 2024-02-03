8b6d53010a70bd3cec1e94008d40a2fc123b2835
Daniel Gonzalez <danielgonzalez@Daniels-MacBook-Pro.local>
2024-02-03 13:05:31 -0800

    expanded gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -2,24 +2,29 @@ import React from 'react'
 
 import './Gallery.css'
 
+import { MdInsertPhoto } from "react-icons/md";
+
 export default function Gallery() {
     // Array of image URLs
     const kidsUrls = [
         './images/homepagephotos/play1.webp',
         './images/homepagephotos/play2.webp',
-        './images/homepagephotos/play3.webp'
+        './images/homepagephotos/play3.webp',
+        './images/homepagephotos/play4.jpg',
+        './images/homepagephotos/play5.jpg',
+        './images/homepagephotos/play6.jpg'
     ];
 
     // Function to render images
     const renderKids = () => {
         return kidsUrls.map((url, index) => (
-            <img className="kidsImages" key={index} src={url} alt={`Image ${index + 1}`} />
+            <img className="kidsImages" key={index} src={url} alt={`Image ${index + 1}`}/>
         ));
     };
 
   return (
     <div style={{padding: '3%'}}>
-        <div cdlassName="images-container">
+        <div style={{display: 'flex', justifyContent: 'center'}}>
             {renderKids()}
         </div>
     </div>
