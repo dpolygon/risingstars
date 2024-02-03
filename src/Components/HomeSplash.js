c940e4dd8a827d0d936312d5bb16c1efae809d61
dpolygon <dannyg@lonestardev.net>
2024-02-03 13:05:31 -0800

    expanded gallery

--- src/Components/HomeSplash.js
+++ src/Components/HomeSplash.js
@@ -2,21 +2,24 @@ import React from "react";
 
 import './HomeSplash.css'
 
+import { HiOutlineArrowSmallDown } from "react-icons/hi2";
+
 export default function HomeSplash() {
     return (
         <div style={{padding: '3%'}}>
             <div className="titleArea">
                 <img src="./images/logo.png" style={{height: '60vh'}}></img>
-                <h1 style={{textAlign: 'center'}}>Bilingual Learning Center</h1>
-                <p className="smallText">Where Little Ones Shine Bright: Nurture, Learn, and Grow at Rising Stars - Where Smart Futures Begin!</p>
+                <h1 style={{marginTop: '-50px', fontSize: '3rem'}}>Bilingual Learning Center</h1>
+                <p>Where Little Ones Shine Bright & Smart Futures Begin!</p>
             </div>
+            <HiOutlineArrowSmallDown style={{display: 'block', bottom: '5%', left: '48.5%', position: 'sticky', color: 'white', fontSize: '3rem'}}/>
             <div style={{
                         display: 'block', 
+                        width: '33%',
                         bottom: '5%', 
                         position: 'sticky', 
                         color: 'white'
-                        }}
-            >
+                        }}>
                 <h2 className='messageTitle'>250 bright stars</h2>
                 <p className="smallText">graduated from our nurturing daycare</p>
             </div>
