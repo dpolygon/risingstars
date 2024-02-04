6d3fde569d605e41d5fba0d60d02e8fa2f76aa23
Daniel Gonzalez <danielgonzalez@Daniels-MacBook-Pro.local>
2024-02-03 16:31:47 -0800

    our value bugfix

--- src/Components/HomeSplash.js
+++ src/Components/HomeSplash.js
@@ -8,8 +8,8 @@ export default function HomeSplash() {
     return (
         <div style={{padding: '3%'}}>
             <div className="titleArea">
-                <img src="./images/logo.png" style={{height: '60vh'}}></img>
-                <h1 style={{marginTop: '-50px', fontSize: '3rem'}}>Bilingual Learning Center</h1>
+                <img src="./images/logo.png" style={{Height: 'auto', width: '35%'}}></img>
+                <h1 style={{textAlign: 'center', marginTop: '-50px', fontSize: '5rem'}}>Bilingual Learning Center</h1>
                 <p>Where Little Ones Shine Bright & Smart Futures Begin!</p>
             </div>
             <HiOutlineArrowSmallDown style={{display: 'block', bottom: '5%', left: '48.5%', position: 'sticky', color: 'white', fontSize: '3rem'}}/>
