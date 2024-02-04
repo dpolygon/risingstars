3925a47934135a7b652bbdd1e56f3bfc937c516e
Daniel Gonzalez <danielgonzalez@Daniels-MacBook-Pro.local>
2024-02-03 17:17:54 -0800

    converted all images to webp

--- src/Components/HomeSplash.js
+++ src/Components/HomeSplash.js
@@ -8,17 +8,24 @@ export default function HomeSplash() {
     return (
         <div style={{padding: '3%'}}>
             <div className="titleArea">
-                <img src="./images/logo.png" style={{Height: 'auto', width: '35%'}}></img>
-                <h1 style={{textAlign: 'center', marginTop: '-50px', fontSize: '5rem'}}>Bilingual Learning Center</h1>
-                <p>Where Little Ones Shine Bright & Smart Futures Begin!</p>
+                <img src="./images/logo.webp" style={{maxHeight: '80%', width: 'auto', marginBottom: '-50px'}}></img>
+                <div style={{textAlign: 'center', backgroundColor: '#2525259e', backdropFilter: 'blur(6px)', borderRadius: '33px', padding: '1%'}}>
+                    <h1 style={{fontSize: '4rem'}}>Bilingual Learning Center</h1>
+                    <p >Where Little Ones Shine Bright & Smart Futures Begin!</p>
+                </div>
             </div>
             <HiOutlineArrowSmallDown style={{display: 'block', bottom: '5%', left: '48.5%', position: 'sticky', color: 'white', fontSize: '3rem'}}/>
             <div style={{
                         display: 'block', 
-                        width: '33%',
+                        width: '12%',
                         bottom: '5%', 
                         position: 'sticky', 
-                        color: 'white'
+                        color: 'white',
+                        backgroundColor: '#2525259e', 
+                        backdropFilter: 'blur(3px)', 
+                        borderRadius: '33px', 
+                        padding: '1%', 
+                        textAlign: 'center'
                         }}>
                 <h2 className='messageTitle'>250 bright stars</h2>
                 <p className="smallText">graduated from our nurturing daycare</p>
