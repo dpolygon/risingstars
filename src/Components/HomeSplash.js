4d84ee59b4df2670bbb9e0da15fe91809c0557fb
dpolygon <dannyg@lonestardev.net>
2024-02-03 18:08:03 -0800

    phone compatible homesplash, values

--- src/Components/HomeSplash.js
+++ src/Components/HomeSplash.js
@@ -8,28 +8,17 @@ export default function HomeSplash() {
     return (
         <div style={{padding: '3%'}}>
             <div className="titleArea">
-                <img src="./images/logo.webp" style={{maxHeight: '80%', width: 'auto', marginBottom: '-50px'}}></img>
-                <div style={{textAlign: 'center', backgroundColor: '#2525259e', backdropFilter: 'blur(6px)', borderRadius: '33px', padding: '1%'}}>
+                <img className='logo' src="./images/logo.webp"></img>
+                <div style={{textAlign: 'center', backgroundColor: '#2525259e', backdropFilter: 'blur(12px)', borderRadius: '33px', padding: '1%'}}>
                     <h1 style={{fontSize: '4rem'}}>Bilingual Learning Center</h1>
                     <p >Where Little Ones Shine Bright & Smart Futures Begin!</p>
                 </div>
             </div>
-            <HiOutlineArrowSmallDown style={{display: 'block', bottom: '5%', left: '48.5%', position: 'sticky', color: 'white', fontSize: '3rem'}}/>
-            <div style={{
-                        display: 'block', 
-                        width: '12%',
-                        bottom: '5%', 
-                        position: 'sticky', 
-                        color: 'white',
-                        backgroundColor: '#2525259e', 
-                        backdropFilter: 'blur(3px)', 
-                        borderRadius: '33px', 
-                        padding: '1%', 
-                        textAlign: 'center'
-                        }}>
+            <div className="message">
                 <h2 className='messageTitle'>250 bright stars</h2>
                 <p className="smallText">graduated from our nurturing daycare</p>
             </div>
+            <HiOutlineArrowSmallDown className="arrow"/>
         </div>
     );
 }
