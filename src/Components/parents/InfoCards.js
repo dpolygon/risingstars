359e63bc28c00c5417ba2a1024f3586f0bf74223
dpolygon <dannyg@lonestardev.net>
2024-02-17 23:16:46 -0800

    adding hover functionality on cards

--- src/Components/parents/InfoCards.js
+++ src/Components/parents/InfoCards.js
@@ -5,30 +5,50 @@ export default function InfoCards() {
   return (
     <div style={{padding: '3%', paddingTop: '80px'}}>
         <ul className="grid">
-            <li className="grid-item">
-                <div></div>
-                <h2>this is the title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+            <li className="cards">
+                <div className='card-content'>
+                    <h2>this is the title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+                </div>
+                <div className='card-content2'> 
+                    <p>youre hovering over me :3</p>
+                </div>
             </li>
-            <li className="grid-item">
-                <div></div>
-                <h2>this is the title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+            <li className="cards">
+                <div className='card-content'>
+                    <h2>this is the title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+                </div>
+                <div className='card-content2'> 
+                    <p>youre hovering over me :3</p>
+                </div>
             </li>
-            <li className="grid-item">
-                <div></div>
-                <h2>this is the title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p> 
+            <li className="cards">
+                <div className='card-content'>
+                    <h2>this is the title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p> 
+                </div>
+                <div className='card-content2'> 
+                    <p>youre hovering over me :3</p>
+                </div>
             </li>
-            <li className="grid-item">
-                <div></div>
-                <h2>this is the title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+            <li className="cards">
+                <div className='card-content'>
+                    <h2>this is the title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+                </div>
+                <div className='card-content2'> 
+                    <p>youre hovering over me :3</p>
+                </div>
             </li>
-            <li className="grid-item">
-                <div></div>
-                <h2>this is the title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+            <li className="cards">
+                <div className='card-content'>
+                    <h2>this is the title</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, nisi!</p>
+                </div>
+                <div className='card-content2'> 
+                    <p>youre hovering over me :3</p>
+                </div>
             </li>
         </ul>
     </div>
