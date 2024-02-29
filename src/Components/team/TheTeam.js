c6ea6ba85e27f9b9708946322ac4c65086bfe6cf
dpolygon <dannyg@lonestardev.net>
2024-02-29 15:37:31 -0600

    edits to member cards font size

--- src/Components/team/TheTeam.js
+++ src/Components/team/TheTeam.js
@@ -12,7 +12,7 @@ export default function TheTeam() {
                 <div style={{backgroundImage: `url(${member.image})`, backgroundSize: 'cover', borderRadius: '12px', backgroundPosition: '50%', fontSize: '12px', fontWeight: '600'}} className='team-member-card' >
                     <div className='team-member-wrapper'>
                         <div className='team-member-card2'>
-                            <p style={{fontSize: '20px', lineHeight: '1.15', }}>{member.summary}</p>
+                            <p style={{fontSize: '18px', lineHeight: '1.15', }}>{member.summary}</p>
                         </div>
                         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 'auto', fontSize: '17px', fontWeight: '400'}}>
                             <p>{member['name']}</p>
