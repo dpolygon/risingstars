366e081f34b1bf646fcb00dddd024a014916313b
dpolygon <dannyg@lonestardev.net>
2024-03-01 18:39:27 -0600

    testing texting

--- src/routes/contactus.js
+++ src/routes/contactus.js
@@ -1,9 +1,30 @@
-import react from 'react'
+import react, { useState } from 'react'
 
 function ContactUs() {
+
+    const [text, setText] = useState({
+        name: "",
+        phoneNumber: "",
+        message: "",
+    });
+
+    const handleStateChange = (e) => {
+        
+    }
+
+    const handleEmailClick = async () => {
+        fetch('/send-text', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify("test luv diva")
+        }) 
+    } 
+    
     return (
         <div className='ContactUs'>
             <h1>Contact Us</h1>
+            <button onClick={handleEmailClick}>click me</button>
+            <a href='sms:5129175055?body=hi'>test</a>
         </div>
     );
 }
