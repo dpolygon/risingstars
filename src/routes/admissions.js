70874d49612bbc2d26ccb824102d7741f016c0c6
dpolygon <dannyg@lonestardev.net>
2024-03-06 00:07:20 -0600

    added proper padding

--- src/routes/admissions.js
+++ src/routes/admissions.js
@@ -14,7 +14,7 @@ function Admissions() {
                     <p style={{textAlign: 'center', fontSize: '17px'}}>Rising Stars is currently accepting new admissions ✅</p>
                 </div>
                 <AdmissionsForm/>
-                <div style={{backgroundColor: 'lightgrey', borderRadius: '33px', padding: '30px', width: '75%'}}>
+                <div style={{backgroundColor: 'lightgrey', borderRadius: '33px', padding: '30px', width: '75%', marginTop: '110px'}}>
                     <p style={{fontSize: '17px'}}>To expedite the enrollment process, parents are welcome to fill out as many forms as they'd like before contacting Rising Stars. We understand your commitment to your child's well-being. To maintain high standards and comply with Texas state law, we kindly request the necessary documents to be signed. We're here to help answer any questions you may have about the documents.</p>
                 </div>
                 <AdmissionsFiles/>
