4cf19ac8f9c07ceb0408ba51975dda327d67724a
Sule Tekin <suleetekin99@gmail.com>
2024-03-24 00:39:55 -0500

    updated page w slideshow

--- src/routes/visitus.js
+++ src/routes/visitus.js
@@ -1,85 +1,94 @@
 // VisitUs.js
-import React from 'react';
-import EmbeddedMap from '../EmbeddedMap';
 import './visitus.css';
+import React, { useState, useEffect } from 'react';
+
 
 function VisitUs() {
 
      // Array of image URLs
+     const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
      const quickImages = [
         './images/visitusphotos/image1.jpeg',
         './images/visitusphotos/image2.jpeg',
-        './images/visitusphotos/image3.jpeg'
+        './images/visitusphotos/image3.jpeg',
+        './images/visitusphotos/image4.jpg'
     ];
 
-    // Function to render images
-    const renderZigzagImages = () => {
-        return quickImages.map((url, index) => (
-          <img className="daycareImages" key={index} src={url} alt={`Image ${index + 1}`} />
-        ));
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex(prevIndex =>
+                prevIndex === quickImages.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 3500); // Change image every 3 seconds
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    // Function to move to the next image
+    const nextImage = () => {
+        setCurrentImageIndex(prevIndex =>
+            prevIndex === quickImages.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    // Function to move to the previous image
+    const prevImage = () => {
+        setCurrentImageIndex(prevIndex =>
+            prevIndex === 0 ? quickImages.length - 1 : prevIndex - 1
+        );
     };
 
 
-  return (
+    return (
 
-    <div id='VisitUs'>
-        <div className='header'>
+        <div id='VisitUs'>
             <div className='columnContainer'>
                 <div className='VSTitle'>
-                    Visit Us
-                </div>
-                <div className='rowContainer'>
-                    <div className='columnContainer'>
-                        <div className='VSTextContainer'>
-                                Located in South Austin, come experience the magic of our daycare! 
-                                    Our doors are open, and we look forward to sharing smiles, laughter, and 
-                                    endless adventures with you and your little ones.
-                        </div>
-                        <div className='formBox'>
-                            <div className='formBox2'>
-                                <div className= 'columnContainer'>
-                                    <div className='formTitle'>
-                                        To reach out to us, please fill in the form below:
-                                    </div>
-
-                                    <div className='formTitle2'>
-                                        Contact Form
-                                    </div>
-
-                                    <hr/>
-                                    <input type='text' placeholder='Name' id ="formName" ></input>
-                                    <input type='text' placeholder='Email' id ="formEmail"></input>
-                                    <input type='text' placeholder='Message' id ="formMessage"></input>
-
-                                    <div className='rowContainer'>
-                                        <div className='ageButton'>Baby</div>
-                                        <div className='ageButton'>Toddler</div>
-                                        <div className='ageButton'>Young Child</div>
-                                        <div className='ageButton'>Big Child</div>
-                                    </div>
-
-                                    <button>
-                                        Send Message
-                                    </button>
-
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className='map'>
-                        < EmbeddedMap/>
-                    </div>
-
-                </div>
+                        Visit Us
+                </div>   
             </div>
+
+
+        <div className='columnContainer'>
+
+            <div className='VSTextContainer'>
+                    Come experience the magic of our daycare! 
+                    Our doors are open, and we look forward to sharing smiles, laughter, and 
+                    endless adventures with you and your little ones.
+            </div>
+        
+        
+            <div className='linkText'>
+                <a href= "/contact-us">Schedule a tour with Rising Stars!</a> 🗓️
+            </div>
+            
+
+            <div className='rowContainer'>
+                <div className="slideshow-container">
+                    <button className= "prevButton" onClick={prevImage}>←</button>
+                        {quickImages.map((imageUrl, index) => (
+                            <div key={index} className={
+                                index === currentImageIndex
+                                    ? 'slide active'
+                                    : 'slide'}>
+                                <img src={imageUrl} alt={`Slide ${index + 1}`} style={{height: "450px", width: "auto", borderRadius: "6px", border: "8px white solid"}} />
+                            </div>
+                        ))}
+                    <button className= "nextButton" onClick={nextImage}>→</button>
+                </div>
+
+            </div>
+        
+            
+            
+
         </div>
-    </div>
 
+        </div>
 
-  );
+    );
 }
 
 export default VisitUs;
-
